4f7b387c83d9f8c682bd1772983c24295d8f4830
zgy <3132701082@qq.com>
2022-10-20 18:21:50 +0800

    inform commit

---
+++ CoHelp/Server/src/main/java/com/cohelp/server/mapper/InformMapper.java
@@ -0,0 +1,18 @@
+package com.cohelp.server.mapper;
+
+import com.cohelp.server.model.entity.Inform;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 县城之子丶
+* @description 针对表【inform(举报表)】的数据库操作Mapper
+* @createDate 2022-10-20 18:15:55
+* @Entity com/cohelp/server.model/entity.Inform
+*/
+public interface InformMapper extends BaseMapper<Inform> {
+
+}
+
+
+
+
---
+++ CoHelp/Server/src/main/java/com/cohelp/server/service/InformService.java
@@ -0,0 +1,13 @@
+package com/cohelp/server.service;
+
+import com/cohelp/server.model/entity.Inform;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 县城之子丶
+* @description 针对表【inform(举报表)】的数据库操作Service
+* @createDate 2022-10-20 18:15:55
+*/
+public interface InformService extends IService<Inform> {
+
+}

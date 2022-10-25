8f30aa1b047eeff009b6c3af7f2bb4e902bd92ff
zgy <3132701082@qq.com>
2022-10-25 11:24:04 +0800

    submitinform commit

--- CoHelp/Server/src/main/java/com/cohelp/server/service/InformService.java
+++ CoHelp/Server/src/main/java/com/cohelp/server/service/InformService.java
@@ -1,14 +1,14 @@
 package com.cohelp.server.service;
 
 import com.cohelp.server.model.domain.Result;
-import com.cohelp.server.model.entity.Inform;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.cohelp.server.model.entity.Inform;
 
 /**
-* @author zgy
-* @description 针对表【inform(举报表)】的数据库操作Service
-* @createDate 2022-10-20 18:15:55
-*/
+ * @author zgy
+ * @description 针对表【inform(举报表)】的数据库操作Service
+ * @createDate 2022-10-20 18:15:55
+ */
 public interface InformService extends IService<Inform> {
     public Result submitInform(Inform inform);
 }

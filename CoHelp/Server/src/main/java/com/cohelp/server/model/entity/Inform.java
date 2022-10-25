0503d6188b345305051ebead11a064e216fbe004
zgy <3132701082@qq.com>
2022-10-25 10:19:38 +0800

    submitinform commit

--- CoHelp/Server/src/main/java/com/cohelp/server/model/entity/Inform.java
+++ CoHelp/Server/src/main/java/com/cohelp/server/model/entity/Inform.java
@@ -6,7 +6,9 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * 举报表
@@ -49,6 +51,8 @@ public class Inform implements Serializable {
     /**
      * 举报时间
      */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+08:00")
     private Date createTime;
 
     @TableField(exist = false)
--- CoHelp/Server/src/main/java/com/cohelp/server/service/ActivityService.java
+++ CoHelp/Server/src/main/java/com/cohelp/server/service/ActivityService.java
@@ -4,9 +4,9 @@ import com.cohelp.server.model.entity.Activity;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
-* @author jianping5
+* @author zgy
 * @description 针对表【activity(活动表)】的数据库操作Service
-* @createDate 2022-10-21 21:22:49
+* @createDate 2022-10-25 10:14:15
 */
 public interface ActivityService extends IService<Activity> {
 

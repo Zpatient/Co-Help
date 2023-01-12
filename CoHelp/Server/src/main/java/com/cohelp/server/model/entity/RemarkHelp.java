e8e1cc0355cf1b500ab8345b67986a62a517299e
zgy <3132701082@qq.com>
2023-01-12 19:08:45 +0800

    update datasource config

--- CoHelp/Server/src/main/java/com/cohelp/server/model/entity/RemarkHelp.java
+++ CoHelp/Server/src/main/java/com/cohelp/server/model/entity/RemarkHelp.java
@@ -4,12 +4,12 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
-import java.util.Date;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 互助评论表

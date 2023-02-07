d1bd5dec6dac4f2f020c0b212d64cd78c84cda74
jianping5 <2712748478@qq.com>
2023-02-07 21:13:24 +0800

    update Help

---
+++ CoHelp/Server/src/main/java/com/cohelp/server/constant/HelpTypeConstant.java
@@ -0,0 +1,39 @@
+package com.cohelp.server.constant;
+
+/**
+ * @author jianping5
+ * @createDate 7/2/2023 下午 8:51
+ */
+public class HelpTypeConstant {
+
+    /**
+     * 全部
+     */
+    public static final String HELP_ALL = "全部";
+
+    /**
+     * 其他
+     */
+    public static final String HELP_OTHER = "其他";
+
+    /**
+     * 组团招人
+     */
+    public static final String HELP_TEAM = "组团招人";
+
+    /**
+     * 寻物启事
+     */
+    public static final String HELP_FIND = "寻物启事";
+
+    /**
+     * 跑腿
+     */
+    public static final String HELP_RUN = "跑腿代取";
+
+    /**
+     * 问题帮助
+     */
+    public static final String HELP_PROBLEM = "问题求助";
+
+}

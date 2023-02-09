50e3427d767e41f6bb39110b115d3bc04b304026
jianping5 <2712748478@qq.com>
2023-02-09 21:35:14 +0800

    update image

--- CoHelp/Server/src/main/java/com/cohelp/server/service/impl/GeneralServiceImpl.java
+++ CoHelp/Server/src/main/java/com/cohelp/server/service/impl/GeneralServiceImpl.java
@@ -22,6 +22,7 @@ import org.wltea.analyzer.core.Lexeme;
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.io.StringReader;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -95,6 +96,9 @@ public class GeneralServiceImpl implements GeneralService {
                     history.setTopicType(type);
                     history.setTopicId(id);
                     historyService.saveOrUpdate(history);
+                } else {
+                    oldHistory.setViewTime(LocalDateTime.now());
+                    historyService.saveOrUpdate(oldHistory);
                 }
                 return ResultUtil.returnResult(SUCCESS_GET_DATA,detailResponse,"数据获取成功！");
             }
